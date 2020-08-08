217d57026bcb0c4b314443fd10a21255ffdc8a94
Carlos Arvelo <carvelog@gmail.com>
2020-08-08 16:48:29 +0100

    basic result component

--- frontend/src/pages/ResultPage/ResultPage.tsx
+++ frontend/src/pages/ResultPage/ResultPage.tsx
@@ -2,11 +2,13 @@ import React, { FC } from "react";
 
 interface ResultPageProps {
   result: number
+  image: string
 }
 
-const ResultPage: FC<ResultPageProps> = ({result}) => {
+const ResultPage: FC<ResultPageProps> = ({result, image}) => {
   return <div>
     <p>Result: {result}</p>
+    <img src={`data:image/jpeg;base64,${image}`} alt="" width="200" height="200"/>
   </div>
 }
 
