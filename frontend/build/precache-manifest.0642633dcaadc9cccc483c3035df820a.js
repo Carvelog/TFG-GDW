e906926a53ef49ccdcc191de2830f61d6680dd31
Carlos Arvelo <carvelog@gmail.com>
2020-08-17 18:25:38 +0100

    .gitignore

---
+++ frontend/build/precache-manifest.0642633dcaadc9cccc483c3035df820a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee4ebd50115ecab6bc1fed9ef66514c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa68282de687e0916a8b",
+    "url": "/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "bf4a72067bd5700a739b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aa68282de687e0916a8b",
+    "url": "/static/js/2.36302c53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.36302c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4a72067bd5700a739b",
+    "url": "/static/js/main.b192936f.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1c59902a584e296f37b6816bee95055c",
+    "url": "/static/media/ULL_logo.1c59902a.svg"
+  },
+  {
+    "revision": "8fdb2e2049b9ef9c14c09c13d54f7699",
+    "url": "/static/media/closeIcon.8fdb2e20.svg"
+  }
+]);
