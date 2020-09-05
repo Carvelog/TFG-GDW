4d35f4da347f7a87c85dfa47434d1973a6adc8ea
Carlos Arvelo <carvelog@gmail.com>
2020-09-05 20:24:35 +0100

    the endpoint to save the image metadata was added

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
 
-from controllers.controllers import ok, process, getDiagnosis, downloadImage
+from controllers.controllers import ok, process, getDiagnosis, downloadImage, saveMetadata
 from config import Config, ProductionConfig
 from database.db import initializeDb
 from nn.modelQueue import load_model, processImage
@@ -25,6 +25,7 @@ api.add_resource(ok, '/api/ok')
 api.add_resource(process, '/api/process')
 api.add_resource(getDiagnosis, '/api/diagnosis/<string:uuid>')
 api.add_resource(downloadImage, '/api/download/<string:uuid>')
+api.add_resource(saveMetadata, '/api/metadata/<string:uuid>')
 
 if __name__ == '__main__':
   app.run()
